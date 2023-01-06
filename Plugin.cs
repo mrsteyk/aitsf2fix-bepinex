cf1165ecce9ee0ef59f74a3d76d61b52531cf84d
Alexandr "MrSteyk" German <mrsteyk1@vk.com>
2023-01-06 19:45:14 +0500

    init

---
+++ Plugin.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.IL2CPP;
+
+using HarmonyLib;
+
+namespace aitsf2fix;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin
+{
+
+    internal static Harmony Harmony { get; } = new Harmony(MyPluginInfo.PLUGIN_GUID);
+
+    private static ConfigEntry<bool> HighMSAA;
+    private static ConfigEntry<int> DesiredResolutionX;
+    private static ConfigEntry<int> DesiredResolutionY;
+    private static ConfigEntry<bool> Fullscreen;
+    private static ConfigEntry<bool> ResolutionOverride;
+    private static ConfigEntry<bool> UIFix;
+    
+    private static BepInEx.Logging.ManualLogSource logger; // Jank???
+
+    public override void Load()
+    {
+        logger = Log;
+        // Plugin startup logic
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+        HighMSAA = Config.Bind("Quality", "HighMSAA", true, "Enable High Quality MSAA on all cameras");
+        
+        DesiredResolutionX = Config.Bind("Resolution", "X", Display.main.systemWidth);
+        DesiredResolutionY = Config.Bind("Resolution", "Y", Display.main.systemHeight);
+        ResolutionOverride = Config.Bind("Resolution", "Override", false);
+        Fullscreen = Config.Bind("Resolution", "Fullscreen", false);
+        UIFix = Config.Bind("Resolution", "UIFix", true);
+
+        Harmony.PatchAll(typeof(Plugin));
+    }
+
+    [HarmonyPatch(typeof(Game.CameraController), nameof(Game.CameraController.OnEnable))]
+    [HarmonyPostfix]
+    public static void CameraQualityFix(Game.CameraController __instance) {
+        if (HighMSAA.Value) {
+            var URPD = __instance._camera.GetComponent<UnityEngine.Rendering.Universal.UniversalAdditionalCameraData>();
+            URPD.antialiasing = UnityEngine.Rendering.Universal.AntialiasingMode.SubpixelMorphologicalAntiAliasing;
+            URPD.antialiasingQuality = UnityEngine.Rendering.Universal.AntialiasingQuality.High;
+            // logger.LogInfo("Set Camera to MSAA High");
+        }
+    }
+
+    [HarmonyPatch(typeof(Game.LauncherArgs), nameof(Game.LauncherArgs.OnRuntimeMethodLoad))]
+    public static void SetResolution() {
+        if (ResolutionOverride.Value) {
+            logger.LogInfo("Overriding resolution");
+            if (Fullscreen.Value) {
+                Screen.SetResolution(DesiredResolutionX.Value, DesiredResolutionY.Value, FullScreenMode.FullScreenWindow);
+                logger.LogInfo("Override to fullscreen");
+            } else {
+                Screen.SetResolution(DesiredResolutionX.Value, DesiredResolutionY.Value, FullScreenMode.Windowed);
+                logger.LogInfo("Override to windowed");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,39 @@
+# AI: THE SOMNIUM FILES - nirvanA Initiative Fix (port to BepInEx6 BE)
+
+This is a WIP BepInEx6 BE version of [AI: THE SOMNIUM FILES - nirvanA Initiative Fix](https://github.com/Lyall/AISomniumFiles2Fix)
+
+## Configuration
+
+```
+## Settings file was created by plugin aitsf2fix v1.0.0
+## Plugin GUID: aitsf2fix
+
+[Quality]
+
+## Enable High Quality MSAA on all cameras
+# Setting type: Boolean
+# Default value: true
+HighMSAA = true
+
+[Resolution]
+
+# Setting type: Int32
+# Default value: Main Display Width
+X = 1920
+
+# Setting type: Int32
+# Default value: Main Display Height
+Y = 1080
+
+# Setting type: Boolean
+# Default value: false
+Override = false
+
+# Setting type: Boolean
+# Default value: false
+Fullscreen = false
+
+# Setting type: Boolean
+# Default value: true
+UIFix = true # doesn't do anything as of rn
+```
